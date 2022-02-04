767feb572adb53f5d6f1da51005ab3bdb1a39dd4
Alexis <alexisvedrenne482@gmail.com>
2022-02-04 18:04:58 +0100

    delete bulma and add boostrap

--- public/assets/js/homeRedirect/home.js
+++
@@ -1,5 +0,0 @@
-const parrallax = document.querySelector('.parralax');
-
-window.addEventListener('scroll',function(){
-    parrallax.style.backgroundPositionY ="-"+this.window.scrollY / 4 + "px"
-})
