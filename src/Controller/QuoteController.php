1ecd11eaf9d68665d00f27ffe1c7b6ece5fcff77
Prince Emmanuel <manu_973@live.fr>
2022-03-09 13:56:51 +0100

    WIP

---
+++ src/Controller/QuoteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuoteController extends AbstractController
+{
+    /**
+     * @Route("/quote", name="app_quote")
+     */
+    public function index(Request $request): Response
+    {
+        
+        return $this->render('quote/index.html.twig', [
+            'controller_name' => 'QuoteController',
+        ]);
+    }
+}
