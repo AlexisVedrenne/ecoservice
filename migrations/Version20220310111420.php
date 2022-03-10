b316589e37911d5e57809da48883182f44fcb711
Alexis <alexisvedrenne482@gmail.com>
2022-03-10 12:14:30 +0100

    création de la mig

--- migrations/Version20220310111420.php
+++ migrations/Version20220310111420.php
@@ -10,11 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-<<<<<<< HEAD:migrations/Version20220310105052.php
-final class Version20220310105052 extends AbstractMigration
-=======
-final class Version20220310110457 extends AbstractMigration
->>>>>>> dev:migrations/Version20220310110457.php
+final class Version20220310111420 extends AbstractMigration
 {
     public function getDescription(): string
     {
