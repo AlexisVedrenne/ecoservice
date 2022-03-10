8a8b473e63ad9825121a2f1bd3885e2ae4168a76
Alexis <alexisvedrenne482@gmail.com>
2022-03-10 11:15:33 +0100

    correction mineur sur le changement de mot de passe

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,6 @@
 footer=document.getElementById('footer')
 footer.style.display='none';
 document.addEventListener('scroll',function(){
-    console.log(window.scrollY)
     if(window.scrollY > 700 || window.scrollY > 500){
         footer.style.display='block';
     }
